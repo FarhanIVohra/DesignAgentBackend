b5c6639240f703b9b55b54f3eaeb94a1d1927b08
FarhanIVohra <worldhello3311@gmail.com>
2025-12-15 09:08:07 +0530

    Update main.py fully

--- main.py
+++ main.py
@@ -19,12 +19,7 @@ app = FastAPI(
 # from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "design-agent-frontend.vercel.app"
-        "https://design-agent-frontend-2s0akhh8n-farhanivohras-projects.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
