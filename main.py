9576001e2e814de84e8d5065e50802e571f60eb3
FarhanIVohra <worldhello3311@gmail.com>
2025-12-15 08:10:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://design-agent-frontend-odyixhvz9-farhanivohras-projects.vercel.app",
+        "https://design-agent-frontend-a430o4csb-farhanivohras-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
